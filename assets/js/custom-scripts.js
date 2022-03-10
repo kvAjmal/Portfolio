8c6bfc56227f0f93bd96f50598e63fd5ad3aaf55
Ajmal <97386008+kvAjmal@users.noreply.github.com>
2022-03-10 10:34:40 +0530

    contact form script updated

--- assets/js/custom-scripts.js
+++ assets/js/custom-scripts.js
@@ -158,53 +158,67 @@
     |=================
     */
         
-      $("#contactForm").validator().on("submit", function (event) {
-          if (event.isDefaultPrevented()) {
-            // handle the invalid form...
-            formError();
-            submitMSG(false, "Did you fill in the form properly?");
-          } else {
-            // everything looks good!
-            event.preventDefault();
-            submitForm();
-          }
-       });
-    
-        function submitForm(){
-          var name = $("#name").val();
-          var email = $("#email").val();
-          var message = $("#message").val();
-          $.ajax({
-              type: "POST",
-              url: "process.php",
-              data: "name=" + name + "&email=" + email + "&message=" + message,
-              success : function(text){
-                  if (text == "success"){
-                      formSuccess();
-                    } else {
-                      formError();
-                      submitMSG(false,text);
-                    }
-                }
-            });
-        }
-        function formSuccess(){
+    $("#contactForm").validator().on("submit", function (event) {
+      if (event.isDefaultPrevented()) {
+          // handle the invalid form...
+          formError();
+          submitMSG(false, "Did you fill in the form properly?");
+      } else {
+          // everything looks good!
+          event.preventDefault();
+          submitForm();
+      }
+    });
+
+
+
+function submitForm(){
+    // Initiate Variables With Form Content
+
+
+    $.ajax({
+        url: "https://script.google.com/macros/s/AKfycbw7Sif9KT3SUZtPKqK6zzh1KrFbr-IavXZEOD53Mw/exec",
+        data:$("#contactForm").serialize(),
+        method:"post",
+
+        success:function (response){
+            formSuccess();
             $("#contactForm")[0].reset();
-            submitMSG(true, "Message Sent!")
+           
+        },
+        error:function (err){
+           formError();
+
         }
-    	  function formError(){   
-    	    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-    	        $(this).removeClass();
-    	    });
-    	  }
-        function submitMSG(valid, msg){
-          if(valid){
-            var msgClasses = "h3 text-center fadeInUp animated text-success";
-          } else {
-            var msgClasses = "h3 text-center shake animated text-danger";
-          }
-          $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
-        }
+           
+    })
+
+}
+
+
+
+function formSuccess(){
+    // $("#contactForm")[0].reset();
+    submitMSG(true, "Message has been submitted successfully.")
+}
+
+
+function formError(){   
+  $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+      $(this).removeClass();
+  });
+}
+
+function submitMSG(valid, msg){
+  if(valid){
+    var msgClasses = "h3 text-center fadeInUp animated text-success";
+  } else {
+    var msgClasses = "h3 text-center shake animated text-danger";
+  }
+  $("#msgSubmit").removeClass().addClass(msgClasses).text(msg);
+}
+
+
     
 
     
